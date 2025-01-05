b04e98a9ddad4647115494a9fc6e447454df95ae
samuelnmu <samuelnmu@gmail.com>
2025-01-05 10:50:50 +0300

    hello.py commit

---
+++ lessonOne/hello.py
@@ -0,0 +1,8 @@
+from cs50 import get_string
+
+print("Hello Sir")
+name = get_string("What is your name? ")
+print(f"Hello, {name}")
+
+food = get_string("What is your favourite meal? ")
+print(f"Intresting, {food} sounds sweet.")
