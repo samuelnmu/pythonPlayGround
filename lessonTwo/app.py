ae34678f4db3e867f2525f8c966a56bc12dfc68c
samuelnmu <samuelnmu@gmail.com>
2025-01-06 21:02:25 +0300

    post method in python

--- lessonTwo/app.py
+++ lessonTwo/app.py
@@ -12,14 +12,14 @@ def index():
     return render_template("index.html")  # Render the index.html template
 
 # Define the route for the greeting page
-@app.route("/greet")
+@app.route("/greet", methods=["POST"])
 def greet():
     """
     Retrieves the user's name from the form and renders the greet.html page.
     If no name is provided, a default name ('Guest') is used.
     """
-    # Get the value of the 'Name' parameter from the form submission
-    name = request.args.get("Name", "Guest")
+    # Get the value of the 'Name' parameter from the form submission (POST method)
+    name = request.form.get("Name", "Guest")
     # Render the greet.html template, passing the name to it
     return render_template("greet.html", name=name)
 
