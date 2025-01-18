7eff34d44e4f333c8d8a00b062d0ea50bdbbc5e4
samuelnmu <samuelnmu@gmail.com>
2025-01-18 22:51:52 +0300

    flight class in py

---
+++ edx/lessonOne/flight.py
@@ -0,0 +1,10 @@
+class Flight(models.Model):
+    origin = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(Airport, on_delete=models.CASCADE, related_name="arrivals")
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
+
+    def is_valid_flight(self):
+        return self.origin != self.destination or self.duration > 0
